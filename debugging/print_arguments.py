c47be90ff33d26b0b74a73ea8cb1f6f507b7dd81
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-01 11:05:22 +0200

    print arguments.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-# If no arguments besides script name, show usage
-if len(sys.argv) < 2:
-    print("Usage: ./args_printer.py <arg1> <arg2> ...")
-    sys.exit(1)
-
-# Enumerate to show index and value
-for index, arg in enumerate(sys.argv):
-    print(f"Argument {index}: {arg}")
+# Iterate over arguments, skipping the script name
+for arg in sys.argv[1:]:
+    print(arg)
